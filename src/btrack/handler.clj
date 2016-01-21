02b6a10696dad3e637ad571093f9b3413eb36e33
Stuart Blair <stuart.a.blair@gmail.com>
2016-01-20 23:09:38 -0800

    Adding middleware to log incoming requests to stdout

--- src/btrack/handler.clj
+++ src/btrack/handler.clj
@@ -1,20 +1,23 @@
 (ns btrack.handler
   (:require [compojure.core :refer :all]
             [compojure.route :as route]
+            ;[compojure.handler :as handler]
+            [ring.util.response :refer [response]]
             [ring.middleware.json :refer [wrap-json-response wrap-json-body]]
             [ring.middleware.defaults :refer [wrap-defaults site-defaults]]))
 
 (defroutes app-routes
-  (route/not-found "{\"message\":\"Page not found\"}"))
-
+           (route/not-found
+             (response {:message "Page not found"})))
 
 (defn wrap-log-request [handler]
   (fn [req]
     (println req)
     (handler req)))
 
-(def app 
+(def app
   (-> app-routes
+      wrap-log-request
       wrap-json-response
       wrap-json-body))
 
